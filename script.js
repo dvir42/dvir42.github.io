6b064ff6eb63b4ccf58e7038df46a75693ef61cb
Yuval <yuvi9313@gmail.com>
2020-03-05 22:13:17 +0200

    Change the d of Days into a capital D

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var x = setInterval(function () {
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        document.getElementById("countdown").innerHTML = days + "D " + hours + "h " + minutes + "m " + seconds + "s ";
         if (!(distance >= 0)) {
 		clearInterval(x);
                 document.getElementById("countdown").innerHTML = "GO!";
