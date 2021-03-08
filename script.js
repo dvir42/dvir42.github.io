2840d86d3c58971985b85f5876a8fb394f9caa7c
Daniel Mortman <danielmortman@gmail.com>
2021-03-08 16:10:46 +0200

    Change date to fit mamas-51 start date

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Nov 29, 2020 08:30:06").getTime();
+var countDownDate = new Date("Apr 25, 2021 08:30:06").getTime();
 var x = setInterval(function () {
 	var now = new Date().getTime();
         var distance = countDownDate - now;
